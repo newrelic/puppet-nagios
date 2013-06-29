6bd9ca52cf314edf0387fef7ca146e3978b0e9a2
Jonathan Thurman <jthurman@newrelic.com>
2013-06-28 19:00:37 -0700

    Automatically restart the nagios service when an exported resource changes

--- manifests/puppet_resources.pp
+++ manifests/puppet_resources.pp
@@ -76,20 +76,20 @@ class nagios::puppet_resources (
 
   if ($auto_collect) {
     # collect resources and populate /etc/nagios/nagios_*.cfg
-    Nagios_command <<||>>
-    Nagios_contact <<||>>
-    Nagios_contactgroup <<||>>
-    Nagios_host <<||>>
-    Nagios_hostdependency <<||>>
-    Nagios_hostescalation <<||>>
-    Nagios_hostextinfo <<||>>
-    Nagios_hostgroup <<||>>
-    Nagios_service <<||>>
-    Nagios_servicedependency <<||>>
-    Nagios_serviceescalation <<||>>
-    Nagios_serviceextinfo <<||>>
-    Nagios_servicegroup <<||>>
-    Nagios_timeperiod <<||>>
+    Nagios_command           <<||>> { notify => Service['nagios'] }
+    Nagios_contact           <<||>> { notify => Service['nagios'] }
+    Nagios_contactgroup      <<||>> { notify => Service['nagios'] }
+    Nagios_host              <<||>> { notify => Service['nagios'] }
+    Nagios_hostdependency    <<||>> { notify => Service['nagios'] }
+    Nagios_hostescalation    <<||>> { notify => Service['nagios'] }
+    Nagios_hostextinfo       <<||>> { notify => Service['nagios'] }
+    Nagios_hostgroup         <<||>> { notify => Service['nagios'] }
+    Nagios_service           <<||>> { notify => Service['nagios'] }
+    Nagios_servicedependency <<||>> { notify => Service['nagios'] }
+    Nagios_serviceescalation <<||>> { notify => Service['nagios'] }
+    Nagios_serviceextinfo    <<||>> { notify => Service['nagios'] }
+    Nagios_servicegroup      <<||>> { notify => Service['nagios'] }
+    Nagios_timeperiod        <<||>> { notify => Service['nagios'] }
   }
 
 }
